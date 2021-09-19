e020a88b438de655262ad05d0458b6f5a98ade8a
ParkSangSun <appleasz1028102@gmail.com>
2021-09-19 18:58:44 +0900

    [ADD] Hilt 추가

---
+++ app/src/main/java/com/ttt/timetoktok/App.kt
@@ -0,0 +1,7 @@
+package com.ttt.timetoktok
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- presentation/src/main/java/com/ttt/presentation/MainActivity.kt
+++ presentation/src/main/java/com/ttt/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ttt.timetoktok
+package com.ttt.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
