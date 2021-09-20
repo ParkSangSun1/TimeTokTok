cfba384286c6df415df3b530b2e770d7456be30f
ParkSangSun <appleasz1028102@gmail.com>
2021-09-20 11:48:34 +0900

    [FIX] Nav 오류, Databinding 오류

--- presentation/src/main/java/com/ttt/presentation/MainActivity.kt
+++ presentation/src/main/java/com/ttt/presentation/MainActivity.kt
@@ -2,14 +2,16 @@ package com.ttt.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.ttt.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    //private lateinit var binding :ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        //binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
     }
 }
